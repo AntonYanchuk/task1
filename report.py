ff92b289096712704e442cd3cf383450d097083d
AntonYanchuk <55788188+AntonYanchuk@users.noreply.github.com>
2020-01-15 13:14:01 +0300

    1

--- report.py
+++ report.py
@@ -3,8 +3,8 @@ import os
 
 bgcolor_mem = '#00ff00'
 bgcolor_proccess = '#00ff00'
-# host_report = os.uname()[1]
-host_report = 'machine'
+host_report = os.uname()[1]
+# host_report = 'machine'
 print(host_report)
 a = psutil.virtual_memory()
 mem_percent = 100.0 - float(a.percent)
@@ -24,37 +24,18 @@ for n in range(disk_count):
     disk_name = disks[n][1]
     b = psutil.disk_usage(disk_name)
     disk_total = round(b.total / (1024.0 ** 3), 2)
-    #    print("Disk: %s, Total Space: %sGB" % (disk_name, disk_total))
     disk_percent = 100.0 - float(b.percent)
     bgcolor_disks = '#00ff00'
     if disk_percent < 20:
         bgcolor_disks = '#FF0000'
-    #    print("Disk: {}, Free Space: {}".format(disk_name, disk_percent))
     disks_value += "<tr><td style=\"width: 155.667px;\">DISK SPACE {disk_name}</td><td style=\"width: 155.667px; text-align: center;\">&nbsp;{disk_total} GB</td><td style=\"width: 155.667px; text-align: center;\">&nbsp;<span style=\"color: {bgcolor_disks};\">{disk_percent}</span></td></tr>\n".format(
         disk_name=disk_name, disk_total=disk_total, disk_percent=disk_percent, bgcolor_disks=bgcolor_disks)
-    #    disks_report.append(value)
-    print(disks_value)
-#    print(value)
-# for i in range(disk_count):
-#    print(disks_report[i])
-# disks_value = str("")
-# for i in range(disk_count):
-#    print(disks_report[i])
-#    disks_value += ", disks{}= disks_report{}\n".format(i, i)
-# print(len(disks_value))
-# print(disks_value)
-# def look():
-
-#    for i in range(disk_count):
 
 
 pid_result = None
 
 for root, dirs, files in os.walk("/opt"):
-    #    print(files)
-    #    print(dirs)
     for file in files:
-        #        print(file)
         if file.endswith("cq.pid"):
             pid_path = os.path.join(root, file)
             f = open(pid_path, "r")
@@ -66,14 +47,11 @@ for root, dirs, files in os.walk("/opt"):
             pid_result = 'false'
             print('Can\'t find AEM pid file')
             bgcolor_proccess = '#8B0000'
-# print(type(pid_result))
+
 if pid_result is None:
     pid_result = 'false'
     print('Can\'t find AEM pid file')
     bgcolor_proccess = '#8B0000'
-# print(pid_result)
-
-# print(disks_report)
 
 message = """
 <html>
